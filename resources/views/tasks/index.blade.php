b843f7f627a9984495ef0c9a8171cb68cb1b45d7
arianndrecaj <ndrecajarian3@gmail.com>
2024-11-12 10:10:34 +0100

    task index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,6 +3,22 @@
 @section('content')
 <div class="container">
     <h1>Your tasks</h1>
+    <a href="{{ route('tasks.create')}}" class="btn btn-primary">Create Task</a>
+    <ul>
+        @foreach ($tasks as task )
+            <li>
+                <h4>{{ $task->title }}</h4>
+                <p>{{ $task->description }}</p>
+                <p>Status: {{ $task->status ? 'Completed' : 'Pending'}}</p>
+                <p>Priority: {{ $task->priority == 1 ? 'High' : ($task->priority == 2 ? 'Medium' : 'Low')}}</p>
+                <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-warning">Edit</a>
+                <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
 </div>
-
 @endsection
