0064d5987551f7bea1206003170c772f6b0b5dbf
arianndrecaj <ndrecajarian3@gmail.com>
2024-11-12 10:22:08 +0100

    added create task blade

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,1 +1,32 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Create Task</h1>
+
+    <form action="{{ route('tasks.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" class="form-control" required></textarea>
+        </div>
+        <div class="form-group">
+            <label for="status">Status</label>
+            <input type="checkbox" name="status">
+        </div>
+        <div class="form-group">
+            <label for="priority">Prioriry</label>
+            <select name="priority" class="form-control">
+                <option value="1">High</option>
+                <option value="2">Medium</option>
+                <option value="3">Low</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-success">Create Task</button>
+    </form>
+</div>
+@endsection
