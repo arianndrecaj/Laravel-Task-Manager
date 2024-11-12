ac8287f3971b759f0ffcf2fe570bfbd959058d8a
arianndrecaj <ndrecajarian3@gmail.com>
2024-11-12 09:22:40 +0100

    added relationship between task and user

--- database/migrations/2024_11_12_081001_create_tasks_table.php
+++ database/migrations/2024_11_12_081001_create_tasks_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('description');
-            $table->timestamps();
             $table->boolean('status')->default(false);
+            $table->integer('priority')->nullable();
+            $table->timestamps();
         });
     }
 
